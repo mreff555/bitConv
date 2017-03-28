8f727895ac4ae4b4b3ad452197cd7de5dc8f054a
mreff555 <feerstd@gmail.com>
2017-03-28 16:43:00 -0400

    Added optional argument to truncate the output string

--- bitConv.cpp
+++ bitConv.cpp
@@ -4,18 +4,20 @@
 
 // Not entirely type safe but it converts
 // any integer to a ledgable binary string
-template <class T>
-std::string bitConv(T in) {
+// the second optional argument will truncate
+// the return value to a specified number of bits
+template <class T> std::string bitConv(T in, unsigned bits = 8*sizeof(T)) {
     std::stringstream ss;
     for( int i = 8*sizeof(T)-1; i >= 0 ; i-- ) {
         ss << ( 0x0001 & (in >> i) );
     }
-    return ss.str();
+    long pos = ss.tellp();
+    return ss.str().substr(pos - bits,bits);
 }
 
 int main() {
 int number = 25;
 long long int num2 = 35;
 std::cout << bitConv(number) <<std::endl;
-std::cout <<bitConv(num2) <<std::endl;
+std::cout <<bitConv(num2,16) <<std::endl;
 }
